c512b890eaac1086c00d44f4f28615cd50fafe5d
Gyorgy Szaszko <adam.szaszko@gmail.com>
2025-07-14 18:45:47 +0200

    updated parser

--- src/parser.c
+++ src/parser.c
@@ -14,7 +14,7 @@
 #pragma GCC optimize ("O0")
 #endif
 
-#define LANGUAGE_VERSION 14
+#define LANGUAGE_VERSION 15
 #define STATE_COUNT 241
 #define LARGE_STATE_COUNT 2
 #define SYMBOL_COUNT 123
@@ -25,7 +25,7 @@
 #define MAX_ALIAS_SEQUENCE_LENGTH 8
 #define MAX_RESERVED_WORD_SET_SIZE 0
 #define PRODUCTION_ID_COUNT 14
-#define SUPERTYPE_COUNT 0
+#define SUPERTYPE_COUNT 1
 
 enum ts_symbol_identifiers {
   anon_sym_cplusplus = 1,
@@ -1236,6 +1236,25 @@ static const TSStateId ts_primary_state_ids[STATE_COUNT] = {
   [238] = 220,
   [239] = 211,
   [240] = 240,
+};
+
+static const TSSymbol ts_supertype_symbols[SUPERTYPE_COUNT] = {
+  sym__targetitem,
+};
+
+static const TSMapSlice ts_supertype_map_slices[] = {
+  [sym__targetitem] = {.index = 0, .length = 7},
+};
+
+static const TSSymbol ts_supertype_map_entries[] = {
+  [0] =
+    anon_sym_AMP,
+    anon_sym_COLON,
+    anon_sym_COLON_COLON,
+    anon_sym_COMMA,
+    anon_sym_DOT,
+    anon_sym_EQ,
+    anon_sym_TILDE,
 };
 
 static bool ts_lex(TSLexer *lexer, TSStateId state) {
@@ -3105,7 +3124,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
   [1] = {.lex_state = 0},
   [2] = {.lex_state = 130},
@@ -7431,6 +7450,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_msg(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
+    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -7438,6 +7458,9 @@ TS_PUBLIC const TSLanguage *tree_sitter_msg(void) {
     .small_parse_table_map = ts_small_parse_table_map,
     .parse_actions = ts_parse_actions,
     .symbol_names = ts_symbol_names,
+    .supertype_map_slices = ts_supertype_map_slices,
+    .supertype_map_entries = ts_supertype_map_entries,
+    .supertype_symbols = ts_supertype_symbols,
     .symbol_metadata = ts_symbol_metadata,
     .public_symbol_map = ts_symbol_map,
     .alias_map = ts_non_terminal_alias_map,
@@ -7445,6 +7468,13 @@ TS_PUBLIC const TSLanguage *tree_sitter_msg(void) {
     .lex_modes = (const void*)ts_lex_modes,
     .lex_fn = ts_lex,
     .primary_state_ids = ts_primary_state_ids,
+    .name = "msg",
+    .max_reserved_word_set_size = 0,
+    .metadata = {
+      .major_version = 0,
+      .minor_version = 0,
+      .patch_version = 1,
+    },
   };
   return &language;
 }
